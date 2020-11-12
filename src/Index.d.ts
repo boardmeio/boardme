cb4307e495a7741c4b2ad285cd3b120afbb00e2e
Andrei Gaspar <andrei@planeeet.com>
2020-11-12 14:13:16 +0100

    Removed src artifacts

--- src/Index.d.ts
+++
@@ -1,2 +0,0 @@
-import { IGuide } from "./models/IGuide";
-export declare function renderGuide(identifier: string, targetElement?: HTMLElement): IGuide | null;
--- src/models/IGuide.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IGuide {
-    identifier: string;
-    name: string;
-    onSubmit(callback: Function): IGuide;
-    onDismiss(callback: Function): IGuide;
-}
