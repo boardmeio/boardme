ed026f1eb1c6c06b95bb113c526996b894e96697
Andrei Gaspar <andrei@planeeet.com>
2020-12-11 18:19:20 +0100

    Set guide behavior auto/manual

--- src/models/IGuide.ts
+++ src/models/IGuide.ts
@@ -4,11 +4,18 @@
 
 
 export interface IGuide {
-    identifier: string,
-    name: string,
-    type: number,
-    onSubmit(callback: Function): IGuide
-    onDismiss(callback: Function): IGuide,
-    show(): IGuide,
-    hide(): IGuide
+    identifier: string;
+    name: string;
+    type: number;
+    onSubmit(callback: Function): IGuide;
+    onDismiss(callback: Function): IGuide;
+    show(): IGuide;
+    hide(): IGuide;
+    setBehavior(behavior: GuideBehavior): void;
+}
+
+
+export enum GuideBehavior {
+    AUTO = "auto",
+    MANUAL = "manual"
 }
